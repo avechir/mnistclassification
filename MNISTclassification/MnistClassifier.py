4e15b964e7ae922ae3fcdc7646473bdb4e4d17ad
avechir <109354328+avechir@users.noreply.github.com>
2025-03-30 21:28:09 +0300

    Add files via upload

---
+++ MNISTclassification/MnistClassifier.py
@@ -0,0 +1,17 @@
+class MnistClassifier:
+    def __init__(self, algorithm):
+        self._algorithm = algorithm  
+
+    @property
+    def algorithm(self):
+        return self._algorithm  
+
+    @algorithm.setter
+    def algorithm(self, algorithm):
+        self._algorithm = algorithm 
+
+    def train(self, X_train, y_train, **kwargs):
+        return self._algorithm.train(X_train, y_train, **kwargs)  
+    
+    def predict(self, X_test):
+        return self._algorithm.predict(X_test)  
---
+++ MNISTclassification/MnistClassifierInterface.py
@@ -0,0 +1,5 @@
+class MnistClassifierInterface:
+    def train(self, bit_des_etalons):
+        pass
+    def predict(self, bit_des_etalons):
+        pass
